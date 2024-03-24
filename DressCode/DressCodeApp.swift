ceaa4559e867ebd688caafaa8287c01059f60c65
Kalindu Washington <kalinduwashington@gmail.com>
2024-03-24 08:52:33 +0530

    Initial Commit

---
+++ DressCode/DressCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DressCodeApp.swift
+//  DressCode
+//
+//  Created by Kalindu Washington on 2024-03-24.
+//
+
+import SwiftUI
+
+@main
+struct DressCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
